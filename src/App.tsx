dd80b6bd54995119980c15c7f22c23f64563c226
liuchenglong <liuchenglong125@foxmail.com>
2025-07-31 00:35:26 +0800

    2025-07-31 00:35:25

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logo from '@/assets/logo.svg'
+function App() {
+	return (
+		<>
+			<h1 className="text-yellow-400">APP</h1>
+			<img src='./assets/logo.svg' />
+		</>
+	)
+}
+
+export default App
