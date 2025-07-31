fa95194623eac07439f1984117dbb8aff843f79b
liuchenglong <liuchenglong125@foxmail.com>
2025-07-31 20:40:05 +0800

    2025-07-31 20:40:05

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,22 @@
+import { Button } from 'antd'
 import type { FC } from 'react'
+import { useNavigate } from 'react-router'
 
 const IndexPage: FC = () => {
-	return <div>IndexPage</div>
+	const navigate = useNavigate()
+	return (
+		<div>
+			<h1>IndexPage</h1>
+			<Button
+				type="primary"
+				onClick={() => {
+					navigate('/demo')
+				}}
+			>
+				to demo
+			</Button>
+		</div>
+	)
 }
 
 export default IndexPage
